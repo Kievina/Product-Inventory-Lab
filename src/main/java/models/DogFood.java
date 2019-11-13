a9f9073d4f116d5012b8acd22c845a3e851231a2
Kievina <56592501+Kievina@users.noreply.github.com>
2019-11-13 16:58:51 -0500

    update model properties

--- src/main/java/models/DogFood.java
+++ src/main/java/models/DogFood.java
@@ -1,4 +1,58 @@
 package models;
 
 public class DogFood {
+    private int id;
+    private String name;
+    private String brand;
+    private float weight;
+    private int qty;
+    private float price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
